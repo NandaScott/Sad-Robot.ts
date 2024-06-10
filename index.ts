a35ad2e70638e445115f16274a0047c6b1eef6ab
NandaScott <nanda1123@gmail.com>
2024-06-10 16:37:54 -0400

    Rewrote interaction handler for ambiguous drop down menus

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ import {
   ButtonBuilder,
   ButtonStyle,
   ComponentType,
+  StringSelectMenuComponent,
+  ActionRow,
 } from 'discord.js';
 import * as response from './test.json';
 import SuccessfulResponseHandler from './src/handlers/SuccessfulResponseHandler/SuccessfulResponseHandler';
@@ -15,6 +17,10 @@ import ScryfallCardModel from './src/types/ScryfallCardModel/ScryfallCardModel';
 import ScryfallResponseError from './src/types/ScryfallResponseError/ScryfallResponseError';
 import AmbiguousResponseBuilder from './src/handlers/AmbiguousResponseHandler/AmbiguousResponseHandler';
 import ErrorsResponseBuilder from './src/handlers/ErrorsResponseBuilder/ErrorsResponseBuilder';
+import SuccessButtonBuilder from './src/handlers/ComponentBuilders/SuccessButtonBuilder';
+import DropDownBuilder from './src/handlers/ComponentBuilders/DropDownBuilder';
+import SuccessRowBuilder from './src/handlers/ComponentBuilders/SuccessRowBuilder';
+import AmbiguousRowBuilder from './src/handlers/ComponentBuilders/AmbiguousRowBuilder';
 
 const mockResponse = response as unknown as {
   successful: CustomResponseData<ScryfallCardModel>[];
@@ -32,9 +38,12 @@ const client = new Client({
 
 client.on(Events.ClientReady, async (event) => {
   console.log(`Logged in as ${client.user?.tag}`);
-  const defaultChannel = await client.channels.fetch('351895958780379148');
+});
 
-  if (defaultChannel?.isTextBased()) {
+client.on(Events.MessageCreate, async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase() === 'test fetch') {
     const successes = new SuccessfulResponseHandler(
       mockResponse
     ).createSuccessRows();
@@ -47,83 +56,103 @@ client.on(Events.ClientReady, async (event) => {
 
     const components = [...successes, ...ambiguous];
 
-    await defaultChannel.send({
+    await message.reply({
       content: 'Here are your cards!',
       components,
       embeds: [...errors],
     });
   }
-});
 
-client.on(Events.MessageCreate, async (message) => {
-  if (message.author.bot) return;
+  // Note: User should only be able to ping 5 cards at a time,
+  // So that if they are all ambiguous returns, discord doesn't try to create
+  // more than 5 action rows.
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
   if (interaction.isStringSelectMenu()) {
     const selectedValue = interaction.values[0];
 
-    const successfulButtons = interaction.message.components
-      .filter((val) => {
-        const componentTypes = val.components.map((comp) => comp.type);
-        if (componentTypes.includes(ComponentType.Button)) return true;
-        return false;
-      })[0]
-      .components.map((comp): any => comp.data)
-      .map((data) => {
-        const [type, id, meta] = data.custom_id.split(':');
-        const newId = `${type}:${Math.random()}:${meta}`;
-        const rebuiltButton = new ButtonBuilder(data);
-        rebuiltButton.setCustomId(newId);
-        return rebuiltButton;
+    // === Start handling success rows ===
+
+    const buttonRows = interaction.message.components
+      // Capture only button rows
+      .filter((row) =>
+        row.components.every((comp) => comp.type === ComponentType.Button)
+      );
+
+    const successRows =
+      buttonRows.length === 0 // Check to see if any button rows exist
+        ? [new SuccessRowBuilder()] // If they don't create a single row builder
+        : buttonRows // Otherwise, work with the rows we have.
+            // Transform component data into data for row builder
+            .map((row) =>
+              row.components.map((comp) => ({
+                ...comp.data,
+                customId: comp.customId,
+              }))
+            )
+            // Create a new set of row builders, and rebuild the button components
+            .map((rowData) => {
+              const row = new SuccessRowBuilder();
+              rowData.forEach((compData) => {
+                if (compData.type === ComponentType.Button) {
+                  if (!compData.label) throw new Error(); // TODO
+                  if (!compData.customId) throw new Error(); // TODO
+
+                  const cardId = compData.customId.split(':').pop();
+                  if (!cardId) throw new Error(); // TODO
+
+                  const button = new SuccessButtonBuilder(
+                    compData.label,
+                    cardId
+                  );
+                  row.addComponent(button.createComponent());
+                }
+              });
+              return row;
+            });
+
+    const lastSuccessRow = successRows.every(
+      (row) => row.row.components.length === 4
+    )
+      ? new SuccessRowBuilder()
+      : successRows.pop();
+
+    if (lastSuccessRow instanceof SuccessRowBuilder) {
+      const newButton = new SuccessButtonBuilder(selectedValue, selectedValue);
+      lastSuccessRow.addComponent(newButton.createComponent());
+    }
+
+    if (lastSuccessRow !== undefined) {
+      successRows.push(lastSuccessRow);
+    }
+
+    const finalSuccesses = successRows.map((row) => row.createComponent());
+
+    // === End handling success rows ===
+
+    // === Start handling ambiguous rows ===
+
+    const dropdownRows = interaction.message.components
+      // Capture only dropdown rows
+      .filter(
+        (row): row is ActionRow<StringSelectMenuComponent> =>
+          row.components[0].data.type === ComponentType.StringSelect &&
+          row.components[0].customId !== interaction.customId
+      )
+      .map((rowData) => {
+        const compData = rowData.components[0];
+        if (compData.placeholder === null) throw new Error();
+
+        const row = new AmbiguousRowBuilder();
+        const dropdown = new DropDownBuilder(compData.placeholder);
+        row.addComponent(dropdown.createComponent());
+        return row.createComponent();
       });
 
-    let foundFurtherErrors = interaction.message.components.filter((val) => {
-      const componentTypes = val.components.map((comp) => comp.type);
-      const componentIds = val.components.map((comp) => comp.customId);
-      if (
-        componentTypes.includes(ComponentType.StringSelect) &&
-        !componentIds.includes(interaction.customId)
-      )
-        return true;
-      return false;
-    });
+    // === End handling ambiguous rows ===
 
-    let furtherErrors: StringSelectMenuBuilder[] = [];
-
-    if (foundFurtherErrors.length > 0) {
-      furtherErrors = foundFurtherErrors[0].components
-        .map((comp): any => comp.data)
-        .map((data) => {
-          const [type, id, meta] = data.custom_id.split(':');
-          const newId = `${type}:${Math.random()}:${meta}`;
-          const rebuiltMenu = new StringSelectMenuBuilder(data);
-          rebuiltMenu.setCustomId(newId);
-          return rebuiltMenu;
-        });
-    }
-
-    const selected = new ButtonBuilder()
-      .setLabel(selectedValue)
-      .setCustomId(`Success:77c6fa74-5543-42ac-9ead-0e890b188e99`)
-      .setStyle(ButtonStyle.Success);
-
-    const buttonRow = new ActionRowBuilder<ButtonBuilder>()
-      .addComponents(successfulButtons)
-      .addComponents(selected);
-
-    const components: (
-      | ActionRowBuilder<ButtonBuilder>
-      | ActionRowBuilder<StringSelectMenuBuilder>
-    )[] = [buttonRow];
-
-    if (furtherErrors.length > 0) {
-      components.push(
-        new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
-          furtherErrors
-        )
-      );
-    }
+    const components = [...finalSuccesses, ...dropdownRows];
 
     await interaction.update({
       components,
